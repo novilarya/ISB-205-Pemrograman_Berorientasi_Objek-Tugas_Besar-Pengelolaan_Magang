6c25b15ad2648b7060130df8f4782e305daac8f8
HilmanNurfauzan <nurfauzan457@gmail.com>
2025-01-13 21:20:23 +0700

    Icon2

--- Tugas_Besar-Pengelolaan_Magang/src/main/java/org/itenas/oop/project/component/DataDiriAdmin.java
+++ Tugas_Besar-Pengelolaan_Magang/src/main/java/org/itenas/oop/project/component/DataDiriAdmin.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 import org.itenas.oop.project.connection.ConnectionManager;
+import org.itenas.oop.project.view.LoginUtama;
 
 /**
  *
@@ -181,8 +182,20 @@ public class DataDiriAdmin extends javax.swing.JPanel {
         try {
             Statement stm = conn.createStatement();
             stm.executeUpdate(query);
-            JOptionPane.showMessageDialog(null, "Update berhasil", "Pesan", JOptionPane.INFORMATION_MESSAGE);        
-        } catch (SQLException ex){
+            JOptionPane.showMessageDialog(null, "Update berhasil silahkan login ulang!", "Pesan", JOptionPane.INFORMATION_MESSAGE);
+            try {
+                conMan = new ConnectionManager();
+                conn = conMan.connectDb();
+                stm.executeUpdate("TRUNCATE TABLE temp_daftar_akun;");
+                new LoginUtama().setVisible(true);
+                javax.swing.JFrame topFrame = (javax.swing.JFrame) javax.swing.SwingUtilities.getWindowAncestor(this);
+                if (topFrame != null) {
+                    topFrame.dispose();
+                }
+            } catch (SQLException ex) {
+                System.out.println("error: " + ex.getMessage());
+            }
+        } catch (SQLException ex) {
             System.out.println("error: " + ex.getMessage());
         }
     }//GEN-LAST:event_btnUpdateActionPerformed
